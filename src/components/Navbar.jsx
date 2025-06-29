a92ff0c62d7a461a2aaa9dc5ae9509de5200f675
sentotAB <sentot.office15@gmail.com>
2025-06-29 22:07:31 +0700

    fixed home button in toggle menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,15 +11,6 @@ const Navbar = () => {
 
   return (
     <header className="sticky top-0 z-50 bg-gradient-to-r from-blue-500 via-gray-900 to-gray-500 text-white shadow-md">
-     {/*  Logo Section */}
-      {/* <div className="absolute top-3 left-20 z-10">
-        <Link
-          to="/" className="text-2xl sm:text-xl hover:bg-black">
-          HOME
-        </Link>
-      </div> */}
-
-    
       {/* Hamburger Icon */}
       <div
         className="absolute top-4 right-4 z-20 flex items-center sm:hidden cursor-pointer"
@@ -35,13 +26,11 @@ const Navbar = () => {
       {/* Desktop Menu */}
       <nav className="flex justify-center items-center px-4 py-4">
         <ul className="hidden sm:flex space-x-8 uppercase text-l tracking-widest">
-        <li>
-            <Link
-              to="/" className=" hover:text-blue-300">
+          <li>
+            <Link to="/" className=" hover:text-blue-300">
               Home
             </Link>
           </li>
-
           <li>
             <Link to="/MyProfile" className="hover:text-blue-300">
               My Profile
@@ -63,6 +52,13 @@ const Navbar = () => {
       {/* Mobile Menu */}
       {isMenuOpen && (
         <div className="absolute top-0 right-0 w-2/3 h-screen bg-black bg-opacity-90 flex flex-col items-center py-16 space-y-6 sm:hidden z-10">
+         <Link 
+            to="/" 
+            className=" hover:text-blue-400"
+            onClick={toggleMenu} 
+            > 
+              Home
+            </Link>
           <Link
             to="/MyProfile"
             className="text-white text-lg hover:text-blue-400"
